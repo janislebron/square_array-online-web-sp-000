7567036198e27e2735b74f309ae68a9b0e19fb62
Janis Lebron <janislebron@gmail.com>
2020-05-09 21:06:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
-def square_array(array)
-    myArray = []
-    array.each do |n|
-        myArray << n ** 2
-    end	
-    new_array
-end
+
+  def square_array(array)	
+      myArray = []	
+      array.each do |n|	
+          myArray << n ** 2	
+      end	
+      new_array	
+  end
